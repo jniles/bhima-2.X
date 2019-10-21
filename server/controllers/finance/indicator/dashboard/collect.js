4a6883d4842e7ff43781714c769fc020d7ba4aa1
Jonathan Niles <jonathanwniles@gmail.com>
2019-10-21 13:57:28 +0100

    chore(indicators): update server code to eslint
    Chore to make indicator server files pass ESLint.  Mostly reformats
    code and removes redundant try/catch blocks.

--- server/controllers/finance/indicator/dashboard/collect.js
+++ server/controllers/finance/indicator/dashboard/collect.js
@@ -2,10 +2,10 @@ const _ = require('lodash');
 const db = require('../../../../lib/db');
 const FilterParser = require('../../../../lib/filter');
 
-module.exports.hospitalization = hospitalization;
-module.exports.finances = finances;
-module.exports.staff = staff;
-module.exports.getProjects = getProjects;
+exports.hospitalization = hospitalization;
+exports.finances = finances;
+exports.staff = staff;
+exports.getProjects = getProjects;
 
 function getDaysOfPeriods(options) {
   const query = `SELECT DATEDIFF(DATE(?), DATE(?)) + 1 AS nb_days;`;
@@ -17,84 +17,77 @@ function getProjects() {
 }
 
 async function hospitalization(options) {
-  try {
-
-    // indicators variables are collected by summarized them according the given periods
-    const sqlSumAggregated = `
-      SELECT 
+  // indicators variables are collected by summarized them according the given periods
+  const sqlSumAggregated = `
+      SELECT
         SUM(IFNULL(hi.total_day_realized, 0)) AS total_day_realized,
         SUM(IFNULL(hi.total_hospitalized_patient, 0)) AS total_hospitalized_patient,
         SUM(IFNULL(hi.total_death, 0)) AS total_death,
         DATE_FORMAT(p.start_date, "%Y-%m-%d") as period_start,
         DATEDIFF(p.end_date, p.start_date) + 1 AS total_period_days,
         s.name as service_name
-      FROM hospitalization_indicator hi  
+      FROM hospitalization_indicator hi
       JOIN indicator ind ON ind.uuid = hi.indicator_uuid
       JOIN period p ON p.id = ind.period_id
       JOIN service s ON s.id = ind.service_id
     `;
 
-    const sqlLastAggregated = `
-      SELECT 
+  const sqlLastAggregated = `
+      SELECT
         SUM(IFNULL(hi.total_beds, 0)) AS total_beds,
         DATE_FORMAT(p.start_date, "%Y-%m-%d") as period_start,
         DATEDIFF(p.end_date, p.start_date) + 1 AS total_period_days,
         s.name as service_name
-      FROM hospitalization_indicator hi  
+      FROM hospitalization_indicator hi
       JOIN indicator ind ON ind.uuid = hi.indicator_uuid
       JOIN period p ON p.id = ind.period_id
       JOIN service s ON s.id = ind.service_id
     `;
 
-    db.convert(options, ['uuid', 'indicator_uuid']);
+  db.convert(options, ['uuid', 'indicator_uuid']);
 
-    // this first filter will be used for the sums query
-    let filters1 = new FilterParser(options, { tableAlias : 'hi' });
+  // this first filter will be used for the sums query
+  let filters1 = new FilterParser(options, { tableAlias : 'hi' });
 
-    // this second filter will be used for the last value query
-    const limitedOptions = !options.groupByPeriod ? _.extend({ limit : 1 }, options) : options;
-    let filters2 = new FilterParser(limitedOptions, { tableAlias : 'hi' });
+  // this second filter will be used for the last value query
+  const limitedOptions = !options.groupByPeriod ? _.extend({ limit : 1 }, options) : options;
+  let filters2 = new FilterParser(limitedOptions, { tableAlias : 'hi' });
 
-    filters1 = defaultFilters(filters1);
-    filters2 = defaultFilters(filters2);
+  filters1 = defaultFilters(filters1);
+  filters2 = defaultFilters(filters2);
 
-    if (options.project_id) {
-      filters1.equals('project_id', 'project_id', 's');
-      filters2.equals('project_id', 'project_id', 's');
-    }
-
-    // group by periods
-    if (options.groupByPeriod) {
-      filters1.setGroup('GROUP BY p.id');
-    }
-
-    // allow to get data of the latest period only for this filter2
-    filters2.setGroup('GROUP BY p.id');
-
-    const sqlSummary = filters1.applyQuery(sqlSumAggregated);
-    const sqlLast = filters2.applyQuery(sqlLastAggregated);
-
-    // sums query
-    const data1 = await db.exec(sqlSummary, filters1.parameters());
-    // last value query
-    const data2 = await db.exec(sqlLast, filters2.parameters());
-    // number of days between end date and start date
-    const totalDaysOfPeriods = await getDaysOfPeriods({ dateFrom : options.dateFrom, dateTo : options.dateTo });
-
-    return { summaryIndicators : data1, lastValueIndicators : data2, totalDaysOfPeriods };
-  } catch (error) {
-    throw error;
+  if (options.project_id) {
+    filters1.equals('project_id', 'project_id', 's');
+    filters2.equals('project_id', 'project_id', 's');
   }
 
+  // group by periods
+  if (options.groupByPeriod) {
+    filters1.setGroup('GROUP BY p.id');
+  }
+
+  // allow to get data of the latest period only for this filter2
+  filters2.setGroup('GROUP BY p.id');
+
+  const sqlSummary = filters1.applyQuery(sqlSumAggregated);
+  const sqlLast = filters2.applyQuery(sqlLastAggregated);
+
+  // sums query
+  const data1 = await db.exec(sqlSummary, filters1.parameters());
+  // last value query
+  const data2 = await db.exec(sqlLast, filters2.parameters());
+  // number of days between end date and start date
+  const totalDaysOfPeriods = await getDaysOfPeriods({ dateFrom : options.dateFrom, dateTo : options.dateTo });
+
+  return { summaryIndicators : data1, lastValueIndicators : data2, totalDaysOfPeriods };
 }
 
 
 async function finances(options) {
-  try {
 
-    // get the sum value for each column
-    const sqlSumAggregated = `
-      SELECT 
+  // get the sum value for each column
+  const sqlSumAggregated = `
+      SELECT
         SUM(IFNULL(fi.total_revenue, 0)) AS total_revenue,
         SUM(IFNULL(fi.total_subsidies, 0)) AS total_subsidies,
         SUM(IFNULL(fi.total_drugs_sale, 0)) AS total_drugs_sale,
@@ -107,61 +100,56 @@ async function finances(options) {
         SUM(IFNULL(fi.total_debts, 0)) AS total_debts,
         DATE_FORMAT(p.start_date, "%Y-%m-%d") as period_start,
         DATEDIFF(p.end_date, p.start_date) AS total_period_days
-      FROM finance_indicator fi  
+      FROM finance_indicator fi
       JOIN indicator ind ON ind.uuid = fi.indicator_uuid
       JOIN period p ON p.id = ind.period_id
     `;
 
-    // indicators variables are collected by considering just values of the last period
-    const sqlLastAggregated = `
-      SELECT 
+  // indicators variables are collected by considering just values of the last period
+  const sqlLastAggregated = `
+      SELECT
         IFNULL(fi.total_cash, 0) AS total_cash,
         IFNULL(fi.total_staff, 0) AS total_staff,
         IFNULL(fi.total_stock_value, 0) AS total_stock_value,
         DATE_FORMAT(p.start_date, "%Y-%m-%d") as period_start,
         DATEDIFF(p.end_date, p.start_date) AS total_period_days
-      FROM finance_indicator fi  
+      FROM finance_indicator fi
       JOIN indicator ind ON ind.uuid = fi.indicator_uuid
       JOIN period p ON p.id = ind.period_id
     `;
 
-    db.convert(options, ['uuid', 'indicator_uuid']);
+  db.convert(options, ['uuid', 'indicator_uuid']);
 
-    // this filter will be used for the sums query
-    let filters1 = new FilterParser(options, { tableAlias : 'fi' });
+  // this filter will be used for the sums query
+  let filters1 = new FilterParser(options, { tableAlias : 'fi' });
 
-    // this second filter will be used for the last value query
-    const limitedOptions = !options.groupByPeriod ? _.extend({ limit : 1 }, options) : options;
-    let filters2 = new FilterParser(limitedOptions, { tableAlias : 'fi' });
+  // this second filter will be used for the last value query
+  const limitedOptions = !options.groupByPeriod ? _.extend({ limit : 1 }, options) : options;
+  let filters2 = new FilterParser(limitedOptions, { tableAlias : 'fi' });
 
-    filters1 = defaultFilters(filters1);
-    filters2 = defaultFilters(filters2);
+  filters1 = defaultFilters(filters1);
+  filters2 = defaultFilters(filters2);
 
-    // groupByPeriod key specify if it is necessary to group by period
-    if (options.groupByPeriod) {
-      filters1.setGroup('GROUP BY p.id');
-    }
-    // sums query
-    const data1 = await db.exec(filters1.applyQuery(sqlSumAggregated), filters1.parameters());
-    // last value query
-    const data2 = await db.exec(filters2.applyQuery(sqlLastAggregated), filters2.parameters());
-    // number of days between end date and start date
-    const totalDaysOfPeriods = await getDaysOfPeriods({ dateFrom : options.dateFrom, dateTo : options.dateTo });
-
-    return { summaryIndicators : data1, lastValueIndicators : data2, totalDaysOfPeriods };
-  } catch (error) {
-    throw error;
+  // groupByPeriod key specify if it is necessary to group by period
+  if (options.groupByPeriod) {
+    filters1.setGroup('GROUP BY p.id');
   }
+  // sums query
+  const data1 = await db.exec(filters1.applyQuery(sqlSumAggregated), filters1.parameters());
+  // last value query
+  const data2 = await db.exec(filters2.applyQuery(sqlLastAggregated), filters2.parameters());
+  // number of days between end date and start date
+  const totalDaysOfPeriods = await getDaysOfPeriods({ dateFrom : options.dateFrom, dateTo : options.dateTo });
 
+  return { summaryIndicators : data1, lastValueIndicators : data2, totalDaysOfPeriods };
 }
 
 
 async function staff(options) {
-  try {
 
-    // get the sum value for each column
-    const sqlSumAggregated = `
-      SELECT 
+  // get the sum value for each column
+  const sqlSumAggregated = `
+      SELECT
         SUM(IFNULL(si.total_external_visit, 0)) AS total_external_visit,
         SUM(IFNULL(si.total_visit, 0)) AS total_visit,
         SUM(IFNULL(si.total_surgery_by_doctor, 0)) AS total_surgery_by_doctor,
@@ -174,48 +162,44 @@ async function staff(options) {
       JOIN period p ON p.id = ind.period_id
     `;
 
-    // indicators variables are collected by considering just values of the last period
-    const sqlLastAggregated = `
-      SELECT 
+  // indicators variables are collected by considering just values of the last period
+  const sqlLastAggregated = `
+      SELECT
         IFNULL(si.total_doctors, 0) AS total_doctors,
         IFNULL(si.total_nurses, 0) AS total_nurses,
         IFNULL(si.total_caregivers, 0) AS total_caregivers ,
         IFNULL(si.total_staff, 0) AS total_staff,
         DATE_FORMAT(p.start_date, "%Y-%m-%d") as period_start,
         DATEDIFF(p.end_date, p.start_date) AS total_period_days
-      FROM staff_indicator si  
+      FROM staff_indicator si
       JOIN indicator ind ON ind.uuid = si.indicator_uuid
       JOIN period p ON p.id = ind.period_id
     `;
 
-    db.convert(options, ['uuid', 'indicator_uuid']);
+  db.convert(options, ['uuid', 'indicator_uuid']);
 
-    // this filter will be used for the sums query
-    let filters1 = new FilterParser(options, { tableAlias : 'si' });
+  // this filter will be used for the sums query
+  let filters1 = new FilterParser(options, { tableAlias : 'si' });
 
-    // this second filter will be used for the last value query
-    const limitedOptions = !options.groupByPeriod ? _.extend({ limit : 1 }, options) : options;
-    let filters2 = new FilterParser(limitedOptions, { tableAlias : 'si' });
+  // this second filter will be used for the last value query
+  const limitedOptions = !options.groupByPeriod ? _.extend({ limit : 1 }, options) : options;
+  let filters2 = new FilterParser(limitedOptions, { tableAlias : 'si' });
 
-    filters1 = defaultFilters(filters1);
-    filters2 = defaultFilters(filters2);
+  filters1 = defaultFilters(filters1);
+  filters2 = defaultFilters(filters2);
 
-    // groupByPeriod only for sqlSumAggregated
-    if (options.groupByPeriod) {
-      filters1.setGroup('GROUP BY p.id');
-    }
-    // sums query
-    const data1 = await db.exec(filters1.applyQuery(sqlSumAggregated), filters1.parameters());
-    // last value query
-    const data2 = await db.exec(filters2.applyQuery(sqlLastAggregated), filters2.parameters());
-    // number of days between end date and start date
-    const totalDaysOfPeriods = await getDaysOfPeriods({ dateFrom : options.dateFrom, dateTo : options.dateTo });
-
-    return { summaryIndicators : data1, lastValueIndicators : data2, totalDaysOfPeriods };
-  } catch (error) {
-    throw error;
+  // groupByPeriod only for sqlSumAggregated
+  if (options.groupByPeriod) {
+    filters1.setGroup('GROUP BY p.id');
   }
+  // sums query
+  const data1 = await db.exec(filters1.applyQuery(sqlSumAggregated), filters1.parameters());
+  // last value query
+  const data2 = await db.exec(filters2.applyQuery(sqlLastAggregated), filters2.parameters());
+  // number of days between end date and start date
+  const totalDaysOfPeriods = await getDaysOfPeriods({ dateFrom : options.dateFrom, dateTo : options.dateTo });
 
+  return { summaryIndicators : data1, lastValueIndicators : data2, totalDaysOfPeriods };
 }
 
 
--- server/controllers/finance/indicator/dashboard/process.js
+++ server/controllers/finance/indicator/dashboard/process.js
@@ -17,37 +17,35 @@ async function processIndicators(options) {
   const dependencies = {};
   const indicators = {};
 
-  try {
+  if (options.distinctProject) {
+    const projects = await collect.getProjects();
+    const hospitalizationByProject = {};
+    const hospitalizationByProjectDependencies = {};
+    indicators.hospitalizationByProject = {};
 
-    if (options.distinctProject) {
-      const projects = await collect.getProjects();
-      const hospitalizationByProject = {};
-      const hospitalizationByProjectDependencies = {};
-      indicators.hospitalizationByProject = {};
+    projects.forEach(async p => {
+      options.project_id = p.id;
+      hospitalizationByProject[p.abbr] = await collect.hospitalization(options);
+      hospitalizationByProjectDependencies[p.abbr] = mergeIndicatorsByPeriod(hospitalizationByProject[p.abbr]);
 
-      projects.forEach(async p => {
-        options.project_id = p.id;
-        hospitalizationByProject[p.abbr] = await collect.hospitalization(options);
-        hospitalizationByProjectDependencies[p.abbr] = mergeIndicatorsByPeriod(hospitalizationByProject[p.abbr]);
-
-        _.keys(hospitalizationByProjectDependencies[p.abbr]).forEach(period => {
-          const periodicDependencies = hospitalizationByProjectDependencies[p.abbr][period];
-          indicators.hospitalizationByProject[p.abbr] = getHospitalizationIndicators(
-            periodicDependencies, hospitalizationByProject[p.abbr].totalDaysOfPeriods.nb_days
-          );
-        });
+      _.keys(hospitalizationByProjectDependencies[p.abbr]).forEach(period => {
+        const periodicDependencies = hospitalizationByProjectDependencies[p.abbr][period];
+        indicators.hospitalizationByProject[p.abbr] = getHospitalizationIndicators(
+          periodicDependencies, hospitalizationByProject[p.abbr].totalDaysOfPeriods.nb_days
+        );
       });
-    }
+    });
+  }
 
-    const hospitalizationCollection = await collect.hospitalization(options);
-    const staffCollection = await collect.staff(options);
-    const financeCollection = await collect.finances(options);
+  const hospitalizationCollection = await collect.hospitalization(options);
+  const staffCollection = await collect.staff(options);
+  const financeCollection = await collect.finances(options);
 
-    dependencies.hospitalization = mergeIndicatorsByPeriod(hospitalizationCollection);
-    dependencies.staff = mergeIndicatorsByPeriod(staffCollection);
-    dependencies.finance = mergeIndicatorsByPeriod(financeCollection);
+  dependencies.hospitalization = mergeIndicatorsByPeriod(hospitalizationCollection);
+  dependencies.staff = mergeIndicatorsByPeriod(staffCollection);
+  dependencies.finance = mergeIndicatorsByPeriod(financeCollection);
 
-    /**
+  /**
      * for the given period range, calculate inducators
      * dependencies are objects with keys as period
      * dependencies :
@@ -61,35 +59,32 @@ async function processIndicators(options) {
      *    }
      * }
      */
-    _.keys(dependencies.hospitalization).forEach(period => {
-      const periodicDependencies = dependencies.hospitalization[period];
-      indicators.hospitalization = getHospitalizationIndicators(
-        periodicDependencies, hospitalizationCollection.totalDaysOfPeriods.nb_days
-      );
-    });
+  _.keys(dependencies.hospitalization).forEach(period => {
+    const periodicDependencies = dependencies.hospitalization[period];
+    indicators.hospitalization = getHospitalizationIndicators(
+      periodicDependencies, hospitalizationCollection.totalDaysOfPeriods.nb_days
+    );
+  });
 
-    // staff indicators
-    _.keys(dependencies.staff).forEach(period => {
-      const periodicDependencies = dependencies.staff[period];
-      indicators.staff = getStaffIndicators(
-        periodicDependencies, staffCollection.totalDaysOfPeriods.nb_days
-      );
-    });
+  // staff indicators
+  _.keys(dependencies.staff).forEach(period => {
+    const periodicDependencies = dependencies.staff[period];
+    indicators.staff = getStaffIndicators(
+      periodicDependencies, staffCollection.totalDaysOfPeriods.nb_days
+    );
+  });
 
-    // finance indicators
-    _.keys(dependencies.finance).forEach(period => {
-      const periodicDependencies = dependencies.finance[period];
-      indicators.finance = getFinanceIndicators(
-        periodicDependencies, financeCollection.totalDaysOfPeriods.nb_days
-      );
-    });
+  // finance indicators
+  _.keys(dependencies.finance).forEach(period => {
+    const periodicDependencies = dependencies.finance[period];
+    indicators.finance = getFinanceIndicators(
+      periodicDependencies, financeCollection.totalDaysOfPeriods.nb_days
+    );
+  });
 
-    const periodicIndicators = await processPeriodicIndicators(options);
+  const periodicIndicators = await processPeriodicIndicators(options);
 
-    return { indicators, periodicIndicators };
-  } catch (error) {
-    throw error;
-  }
+  return { indicators, periodicIndicators };
 }
 
 /**
@@ -101,52 +96,47 @@ async function processPeriodicIndicators(options) {
   const dependencies = {};
   const indicators = {};
 
-  try {
+  // hospitalization indicators
+  const hospitalizationCollection = await collect.hospitalization(options);
+  const staffCollection = await collect.staff(options);
+  const financeCollection = await collect.finances(options);
 
-    // hospitalization indicators
-    const hospitalizationCollection = await collect.hospitalization(options);
-    const staffCollection = await collect.staff(options);
-    const financeCollection = await collect.finances(options);
+  dependencies.hospitalization = mergeIndicatorsByPeriod(hospitalizationCollection);
+  dependencies.staff = mergeIndicatorsByPeriod(staffCollection);
+  dependencies.finance = mergeIndicatorsByPeriod(financeCollection);
 
-    dependencies.hospitalization = mergeIndicatorsByPeriod(hospitalizationCollection);
-    dependencies.staff = mergeIndicatorsByPeriod(staffCollection);
-    dependencies.finance = mergeIndicatorsByPeriod(financeCollection);
+  // hospitalization
+  indicators.periodicHospitalization = {};
+  _.keys(dependencies.hospitalization).forEach(period => {
+    const periodicDependencies = dependencies.hospitalization[period];
+    indicators.periodicHospitalization[period] = getHospitalizationIndicators(
+      periodicDependencies, hospitalizationCollection.totalDaysOfPeriods.nb_days, period
+    );
+  });
 
-    // hospitalization
-    indicators.periodicHospitalization = {};
-    _.keys(dependencies.hospitalization).forEach(period => {
-      const periodicDependencies = dependencies.hospitalization[period];
-      indicators.periodicHospitalization[period] = getHospitalizationIndicators(
-        periodicDependencies, hospitalizationCollection.totalDaysOfPeriods.nb_days, period
-      );
-    });
+  // staff
+  indicators.periodicStaff = {};
+  _.keys(dependencies.staff).forEach(period => {
+    const periodicDependencies = dependencies.staff[period];
+    indicators.periodicStaff[period] = getStaffIndicators(
+      periodicDependencies, staffCollection.totalDaysOfPeriods.nb_days, period
+    );
+  });
 
-    // staff
-    indicators.periodicStaff = {};
-    _.keys(dependencies.staff).forEach(period => {
-      const periodicDependencies = dependencies.staff[period];
-      indicators.periodicStaff[period] = getStaffIndicators(
-        periodicDependencies, staffCollection.totalDaysOfPeriods.nb_days, period
-      );
-    });
+  // finance
+  indicators.periodicFinance = {};
+  _.keys(dependencies.finance).forEach(period => {
+    const periodicDependencies = dependencies.finance[period];
+    indicators.periodicFinance[period] = getFinanceIndicators(
+      periodicDependencies, financeCollection.totalDaysOfPeriods.nb_days, period
+    );
+  });
 
-    // finance
-    indicators.periodicFinance = {};
-    _.keys(dependencies.finance).forEach(period => {
-      const periodicDependencies = dependencies.finance[period];
-      indicators.periodicFinance[period] = getFinanceIndicators(
-        periodicDependencies, financeCollection.totalDaysOfPeriods.nb_days, period
-      );
-    });
-
-    return {
-      hospitalization : formatIndicatorsPeriodicValues(indicators.periodicHospitalization),
-      staff : formatIndicatorsPeriodicValues(indicators.periodicStaff),
-      finance : formatIndicatorsPeriodicValues(indicators.periodicFinance),
-    };
-  } catch (error) {
-    throw error;
-  }
+  return {
+    hospitalization : formatIndicatorsPeriodicValues(indicators.periodicHospitalization),
+    staff : formatIndicatorsPeriodicValues(indicators.periodicStaff),
+    finance : formatIndicatorsPeriodicValues(indicators.periodicFinance),
+  };
 }
 
 /**
@@ -516,5 +506,6 @@ function getFinanceIndicators(dependencies, nbDays = 356, period) {
       period : period || undefined,
     },
   };
+
   return indicators;
 }
